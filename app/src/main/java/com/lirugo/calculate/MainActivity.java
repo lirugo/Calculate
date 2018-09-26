d7ba4fd620062273132e545fbe68d69003acfe1c
lirguo <roostik21@gmail.com>
2018-09-26 15:27:01 +0300

    Adding bottom app bar

--- app/src/main/java/com/lirugo/calculate/MainActivity.java
+++ app/src/main/java/com/lirugo/calculate/MainActivity.java
@@ -1,7 +1,12 @@
 package com.lirugo.calculate;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +14,35 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Set using menu for bottom bar
+        Toolbar toolbar = (Toolbar) findViewById(R.id.bottom_app_bar);
+        setSupportActionBar(toolbar);
+    }
+
+    //Init options for menu
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.bottom_app_bar_menu, menu);
+        return true;
+    }
+
+    //Set simplify action on click item in menu
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        switch (item.getItemId()) {
+            case R.id.app_bar_fav :
+                Toast.makeText(this, "I'm like it", Toast.LENGTH_SHORT).show();
+            case R.id.app_bar_clear :
+                Toast.makeText(this, "Clear all", Toast.LENGTH_SHORT).show();
+        }
+
+        return true;
+    }
+
+    //Action on click fab button
+    public void onClickAddButton(View view) {
+        Toast.makeText(this, "Add toast", Toast.LENGTH_SHORT).show();
     }
 }
